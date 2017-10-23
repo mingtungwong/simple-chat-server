bca4ca8884e8a9e97dc52239444a9b4d729728a3
Ming Tung Wong <mingtungwong@outlook.com>
2017-10-23 12:54:55 -0400

    Implement adding of users

--- Chat.java
+++ Chat.java
@@ -8,4 +8,16 @@ public class Chat {
         users = new ArrayList<User>();
         messages = new ArrayList<Message>();
     }
+
+    public void addUser(String uName) throws Exception {
+        if(!hasUser(uName)) users.add(new User(uName));
+        else throw new Exception("The user already exists");
+    }
+
+    public boolean hasUser(String uName) {
+        for(User u: users) {
+            if(u.getUsername() == uName) return true;
+        }
+        return false;
+    }
 }
--- User.java
+++ User.java
@@ -4,17 +4,11 @@ public class User {
     public User(String uName) {
         username = uName;
     }
-
-	/**
-	 * @return the username
-	 */
+    
 	public String getUsername() {
 		return username;
 	}
 
-	/**
-	 * @param username the username to set
-	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
